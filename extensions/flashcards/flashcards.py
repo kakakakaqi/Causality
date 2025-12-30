f49a823f3d80a303da5251f4028be7f853dc6a49
Jerry <1465087233@qq.com>
2025-12-30 17:01:45 +0800

    initial commit of flashcards
    implemented direct attribute questions

--- extensions/flashcards/flashcards.py
+++ extensions/flashcards/flashcards.py
@@ -1,13 +1,37 @@
 # flashcards essentially ask about the edges of the graph
 # from typing import TYPE_CHECKING
+from dataclasses import dataclass
 import os
 from causality_lang import Node, Node_manager, Graph
 
-node_manager: Node_manager | None = None
+
+@dataclass
+class Question:
+    question: str
+    answer: str
 
 
-def load(nodes: Node_manager):
-    node_manager = nodes
+_graph: Graph | None = None
+_questions: list[Question] = []
+
+
+def load(graph: Graph):
+    global _graph
+    _graph = graph
+
+
+def attribute_questions(graph: Graph | None = None):
+    graph = graph or _graph
+    assert graph is not None
+    nodes = graph.nodes
+    for node in nodes.get_all():
+        for child in node.children.get_all():
+            _questions.append(
+                Question(
+                    question=f"{node.content} . {child.name} = ?",
+                    answer=f"{child.content}",
+                )
+            )
 
 
 def health_check():
@@ -20,10 +44,13 @@ def health_check():
     """
     (g := Graph()).parse(full_code)
     node_manager = g.nodes
-    load(g.nodes)
+    load(g)
     assert node_manager is not None
+    global nodes
     nodes = node_manager.get_all()
+    attribute_questions(g)
 
 
 if __name__ == "__main__":
     health_check()
+    print(_questions)
